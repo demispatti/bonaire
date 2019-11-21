a7c23387c9bb4159655f19baeb71ab9a1313811f
demis.patti <demis.patti@DEMIS-PC>
2019-11-21 22:47:13 +0100

    Update files

--- README.md
+++ README.md
@@ -1,28 +1,32 @@
-=== Bonaire (beta) ===  
-Contributors: Demis Patti  
-Tags: Reply, Messages, Contact Form 7, Flamingo, Email, Send, Send Reply  
+=== Bonaire ===  
+Author: Demis Patti  
+Tags: Contact Form 7, Flamingo, Message, Reply, Email, E-Mail, Send, Send Mail, Send E-Mail  
 Requires at least: 5.1  
 Tested up to: 5.3  
-Stable tag: 0.9.6  
+Stable tag: 1.0.0  
 License: GPLv2 or later  
 License URI: http://www.gnu.org/licenses/gpl-2.0.html  
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=XLMMS7C62S76Q  
+
 Send Replies to messages received trough 'Contact Form 7' and stored with 'Flamingo'.  
 
-== Description ==
+== Description ==  
 
-In case you are using both 'Contact Form 7' and 'Flamingo', with Bonaire you can send Replies to messages you received trough a CF7 form and have stored with 'Flamingo'. In addition, Bonaire let's you store your replies in your mail server's "Sent Items" folder. Never lose track of a conversation! Supports one contact form per website (for now).
+In case you are using both 'Contact Form 7' and 'Flamingo':  
+Send Replies to messages you received trough a CF7 form and have stored with 'Flamingo'. In addition, Bonaire let's you store your replies in your mail server's "Sent Items" folder. Never lose track of a conversation.
 
 == Features ==
 
-- Send replies to messages
-- Store replies on your mail server's 'Sent Items' folder
-- Dashboard Widget
+- Send replies to messages recieved trough a default "Contact Form 7" contact form
+- Store replies on your mail server's 'Sent Items' folder  
+- Dashboard Widget lists incoming messages  
+- Email replies are text-only (HTML-Mails will be added soon)  
 
 == Requirements ==
 
 - A 'Contact Form 7' contact form.
-- 'Flamingo' needs to be installed and activated.
+- 'Flamingo', installed and activated.
+- PHP IMAP extension installed and enabled on your web server  
 
 PHP version 5.6 or above.
 
@@ -37,7 +41,7 @@ PHP version 5.6 or above.
 
 Please visit the plugin help tab for further information.
 
-= Why doesn't it show up in the settings menu? =
+= Why doesn't it show up in the settings menu after installing and activating? =
 
 Most likely, this is because you didn't install and activate the plugins Bonaire was made for, namely 'Contact Form 7' and 'Flamingo'.
 After installing and activating these two plugins, Bonaire will show up on in the settings menu.
@@ -46,19 +50,21 @@ After installing and activating these two plugins, Bonaire will show up on in th
 
 That's unlikely.
 First of all, you have to enter the email account settings for the email account that you use on the contact form. You can do that on the settings page "Settings -> Bonaire".
-If you want to store your replies in your mail server's "Sent Items" folder, you have to enter your IMAP settings, and maybe activate the 'nocert' option, too.
+If you want to store your replies in your mail server's "Sent Items" folder, you have to enter your IMAP settings, too.
 
 = Are there any known limitations? =
 
-- There is no support for attachments (yet)
+- Handling attachmants is not supported
+- Simple plain text email format for now
+- Supports one contact form for now
 
 = Can you help me? =
 
-Yes. I have a look at the plugin's support page two or three times a month and I provide some basic support there.
+Yes. I have a look at the plugin's support page once or twice a month and I provide some basic support there.
 
-= Are there any known issues? =
+= Are there any known issues? =  
 
-- No
+No.
 
 == Screenshots ==
 
@@ -68,5 +74,5 @@ Yes. I have a look at the plugin's support page two or three times a month and I
 
 == Changelog ==
 
-= Version 0.9.6 =
+= Version 1.0.0 =
 1. First commit
--- bonaire.php
+++ bonaire.php
@@ -1,5 +1,16 @@
 <?php
 
+namespace Bonaire;
+
+use Bonaire\Includes as Includes;
+
+/**
+ * If this file is called directly, abort.
+ */
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
 /**
  * The plugin bootstrap file
  * This file is read by WordPress to generate the plugin information in the plugin
@@ -7,13 +18,12 @@
  * registers the activation and deactivation functions, and defines a function
  * that starts the plugin.
  *
- * @link
  * @since             1.0.0
  * @package           Bonaire
  * @wordpress-plugin
  * Plugin Name:       Bonaire
  * Plugin URI:
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Description:       Send Replies to messages you receive trough a 'Contact Form 7' contact form and stored with 'Flamingo'.
  * Version:           1.0.0
  * Author:            Demis Patti
  * Author URI:
@@ -23,10 +33,125 @@
  * Domain Path:       /languages
  */
 
-// If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
+/**
+ * Define plugin constants.
+ */
+if ( ! defined( 'BONAIRE_ROOT_DIR' ) ) {
+	define( 'BONAIRE_ROOT_DIR', plugin_dir_path( __FILE__ ) . '/' );
+}
+if ( ! defined( 'BONAIRE_ROOT_URL' ) ) {
+	define( 'BONAIRE_ROOT_URL', plugin_dir_url( __FILE__ ) . '/' );
 }
 
-// Include the plugin.
-require plugin_dir_path( __FILE__ ) . 'src/bonaire.php';
+/**
+ * Include dependencies.
+ */
+if ( ! class_exists( 'Includes\Bonaire' ) ) {
+	require_once BONAIRE_ROOT_DIR . 'includes/class-bonaire.php';
+}
+if ( ! class_exists( 'Includes\Bonaire_Activator' ) ) {
+	require_once BONAIRE_ROOT_DIR . 'includes/class-activator.php';
+}
+if ( ! class_exists( 'Includes\Bonaire_Deactivator' ) ) {
+	require_once BONAIRE_ROOT_DIR . 'includes/class-deactivator.php';
+}
+
+/**
+ * The class that launches the plugin.
+ *
+ * @since     0.9.6
+ * @package    Bonaire
+ * @subpackage
+ * @author     Demis Patti <demispatti@gmail.com>
+ */
+class Bonaire_Launcher {
+	
+	/**
+	 * The name of the plugin.
+	 *
+	 * @var      string $name
+	 * @since   0.9.6
+	 * @access   public
+	 */
+	public $name = 'bonaire';
+	
+	/**
+	 * The domain of the plugin.
+	 *
+	 * @var      string $domain
+	 * @since   0.9.6
+	 * @access   public
+	 */
+	public $domain = 'bonaire';
+	
+	/**
+	 * The version of the plugin.
+	 *
+	 * @var      string $version
+	 * @since   0.9.6
+	 * @access   public
+	 */
+	public $version = '1.0.0';
+	
+	/**
+	 * Registers the methods that need to be hooked with WordPress.
+	 *
+	 * @since 0.9.6
+	 * @return void
+	 */
+	public function add_hooks() {
+		
+		register_activation_hook( __FILE__, array( $this, 'activate_bonaire' ) );
+		register_deactivation_hook( __FILE__, array( $this, 'deactivate_bonaire' ) );
+	}
+	
+	/**
+	 * Runs during plugin activation.
+	 * This action is documented in includes/class-activator.php
+	 *
+	 * @since 0.9.6
+	 * @return void
+	 */
+	public function activate_bonaire() {
+		
+		$Bonaire_Activator = new Includes\Bonaire_Activator();
+		$Bonaire_Activator->activate();
+	}
+	
+	/**
+	 * Runs during plugin deactivation.
+	 * This action is documented in includes/class-deactivator.php
+	 *
+	 * @since 0.9.6
+	 * @return void
+	 */
+	public function deactivate_bonaire() {
+		
+		$Bonaire_Deactivator = new  Includes\Bonaire_Deactivator();
+		$Bonaire_Deactivator->deactivate();
+	}
+	
+	/**
+	 * Begins execution of the plugin.
+	 *
+	 * @since 0.9.6
+	 * @return void
+	 */
+	public function run_bonaire() {
+		
+		$Bonaire = new Includes\Bonaire( $this->name, $this->domain, $this->version );
+		$Bonaire->init();
+	}
+	
+}
+
+/**
+ * Add activation and deactivation hooks.
+ */
+$Bonaire_Launcher = new Bonaire_Launcher();
+$Bonaire_Launcher->add_hooks();
+
+/**
+ * Run plugin.
+ */
+add_action( 'plugins_loaded', array( $Bonaire_Launcher, 'run_bonaire' ), 10 );
